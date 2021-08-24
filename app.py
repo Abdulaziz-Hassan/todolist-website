ea30e15ab7ed168e7d395bed840d8de8ac9fc4ce
Abdulaziz Hassan Alamri <abdulazizhza@gmail.com>
2021-08-25 01:06:48 +0200

    fix: database

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ Bootstrap(app)
 ckeditor = CKEditor(app)
 
 # Database Connection
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///todolist.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.app = app
 db.init_app(app)
