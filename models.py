4fe7cadea661e8709a05327f5942cf0c14f069e4
Abdulaziz Hassan Alamri <abdulazizhza@gmail.com>
2021-08-24 23:26:34 +0200

    refactor: models

--- models.py
+++ models.py
@@ -3,18 +3,20 @@ from flask_login import UserMixin
 from database import db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
-    todo_items = db.relationship("ToDoItem", backref="author")
+    todo_items = relationship("ToDoItem", back_populates="author")
 
 
 class ToDoItem(db.Model):
-    # __tablename__ = "todo_items"
+    __tablename__ = "items"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(75), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="todo_items")
     is_completed = db.Column(db.Boolean, default=False, nullable=False)
