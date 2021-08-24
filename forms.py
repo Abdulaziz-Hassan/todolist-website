de613afd2dfeebd34dd1eb1b76b8b0c78dec3805
Abdulaziz Hassan Alamri <abdulazizhza@gmail.com>
2021-08-24 19:54:16 +0200

    feat: flash error messages and change completed item color

--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 from flask_ckeditor import CKEditorField
 
 
 class RegisterForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email(message=f"Enter a valid email.")])
-    password = PasswordField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=35)])
     recaptcha = RecaptchaField()
     submit = SubmitField("Register")
 
