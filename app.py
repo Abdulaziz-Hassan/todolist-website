0ca33333d4d88bf5fd9a8e65c45d270a8ad66601
Abdulaziz Hassan Alamri <abdulazizhza@gmail.com>
2021-08-25 01:38:51 +0200

    fix: database connection

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ from dotenv import load_dotenv
 from database import db
 from models import User, ToDoItem
 from forms import RegisterForm, LoginForm, TODOItemForm
+import requests
 import os
 
 app = Flask(__name__)
+# TODO dashboard mobile responsive
 
 load_dotenv()
 
@@ -49,9 +51,15 @@ def home():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
+        # Email Validation
+        response = requests.get("https://isitarealemail.com/api/email/validate", params={"email": form.email.data})
+        status = response.json()["status"]
         if User.query.filter_by(email=form.email.data).first():
             flash("You,ve already signed up with that email, log in instead!")
             return redirect(url_for("login"))
+        elif not status == "valid":
+            flash("Please enter a valid email address.")
+            return redirect(url_for("register"))
         hashed_password = generate_password_hash(
             form.password.data,
             method="pbkdf2:sha256",
